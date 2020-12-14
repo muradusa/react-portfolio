c6e4858e6d048f398a29c889442a516dc3f5b2e9
Murad Cholukov <muradusa@gmail.com>
2020-12-14 13:22:04 -0800

    adding experience section

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Header.css";
 import { Link } from "react-scroll";
 
+// delet later
+
 function Header() {
   const [show, handleShow] = useState(false);
 
