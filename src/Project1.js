2defe623c25fa2a239e27b7feeca46ba25c9c385
Murad Cholukov <muradusa@gmail.com>
2021-01-12 07:58:59 -0800

    making responsive

--- src/Project1.js
+++ src/Project1.js
@@ -28,11 +28,6 @@ function Project1() {
               expertise
             </li>
           </ul>
-          {/* <p>
-            A nicer look at your GitHub profile and repository stats with data
-            visualizations of your top languages and stars. Sort through your
-            top repos by number of stars, forks, and size.
-          </p> */}
         </div>
 
         <div className="info__tech">
--- src/Project2.js
+++ src/Project2.js
@@ -9,14 +9,22 @@ function Project2() {
     <div className="project">
       <div className="info__O">
         <div className="info__title">
-          <p>USA-TM-Chat</p>
+          <p>USA TM Chat</p>
         </div>
         <div className="info__description">
-          <p>
-            A nicer look at your GitHub profile and repository stats with data
-            visualizations of your top languages and stars. Sort through your
-            top repos by number of stars, forks, and size.
-          </p>
+          <ul className="experience__item">
+            <li>
+              Worked with a team of three designers to build a marketing website
+            </li>
+            <li>
+              Helped solidify a brand direction for blistabloc that spans both
+              packaging and web
+            </li>
+            <li>
+              Interfaced with clients on a weekly basis, providing technological
+              expertise
+            </li>
+          </ul>
         </div>
         <div className="info__tech">
           <p>React.js</p>
