b44db598e95c07d632432a4e265e838a79f22a8e
Murad Cholukov <muradusa@gmail.com>
2021-01-27 08:19:16 -0800

    making responsive

--- src/Contact.js
+++ src/Contact.js
@@ -12,80 +12,82 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function Contact() {
   return (
-    <div className="contact" id="contact">
-      <div className="contact__header">
-        <span style={{ color: "royalblue", fontSize: "25px" }}>04.</span>
-        <h1 style={{ paddingLeft: "15px", paddingRight: "15px" }}>
-          Get In Touch
-        </h1>
-        <span className="titleLine"></span>
-      </div>
-
-      <div
-        className="contact__body"
-        style={{
-          backgroundImage: `url(${contactBanner}`,
-        }}
-      >
-        <div className="contact__form">
-          <form name="contact" action="/contact" method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <p>Name</p>
-            <input required type="text" name="name" placeholder="Your Name" />
-            <p>Email</p>
-            <input
-              required
-              type="email"
-              name="email"
-              placeholder="Your Email"
-            />
-            <p>Subject</p>
-            <input name="subject" type="text" placeholder="Subject" />
-            <p>Message</p>
-            <textarea
-              required
-              name="message"
-              placeholder="Message"
-              cols="30"
-              rows="10"
-            ></textarea>
-            <button type="submit" className="contact__button">
-              Send
-            </button>
-          </form>
+    <div className="main__contact">
+      <div className="contact" id="contact">
+        <div className="contact__header">
+          <span style={{ color: "royalblue", fontSize: "25px" }}>04.</span>
+          <h1 style={{ paddingLeft: "15px", paddingRight: "15px" }}>
+            Get In Touch
+          </h1>
+          <span className="titleLine"></span>
         </div>
-        <div className="contact__text">
-          <p>I am open for new opportunities. Let's connect.</p>
-          <div className="contact__social">
-            <a target="_blank" href="https://github.com/muradusa/">
-              <GitHubIcon />
-            </a>
-            <a target="_blank" href="https://www.instagram.com/murad_webdev/">
-              <InstagramIcon />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/muradcholukov/"
-            >
-              <LinkedInIcon />
-            </a>
-            <a target="_blank" href="https://twitter.com/muradusa">
-              <TwitterIcon />
-            </a>
+
+        <div
+          className="contact__body"
+          style={{
+            backgroundImage: `url(${contactBanner}`,
+          }}
+        >
+          <div className="contact__form">
+            <form name="contact" action="/contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
+              <p>Name</p>
+              <input required type="text" name="name" placeholder="Your Name" />
+              <p>Email</p>
+              <input
+                required
+                type="email"
+                name="email"
+                placeholder="Your Email"
+              />
+              <p>Subject</p>
+              <input name="subject" type="text" placeholder="Subject" />
+              <p>Message</p>
+              <textarea
+                required
+                name="message"
+                placeholder="Message"
+                cols="30"
+                rows="10"
+              ></textarea>
+              <button type="submit" className="contact__button">
+                Send
+              </button>
+            </form>
+          </div>
+          <div className="contact__text">
+            <p>I am open for new opportunities. Let's connect.</p>
+            <div className="contact__social">
+              <a target="_blank" href="https://github.com/muradusa/">
+                <GitHubIcon />
+              </a>
+              <a target="_blank" href="https://www.instagram.com/murad_webdev/">
+                <InstagramIcon />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/muradcholukov/"
+              >
+                <LinkedInIcon />
+              </a>
+              <a target="_blank" href="https://twitter.com/muradusa">
+                <TwitterIcon />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="upArrow">
-        <Link
-          activeClass="active"
-          to="home"
-          spy={true}
-          smooth={true}
-          offset={-10}
-          duration={500}
-        >
-          <ArrowUpwardIcon className="arrowUpwardIcon" fontSize="30px" />
-        </Link>
+        <div className="upArrow">
+          <Link
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={-10}
+            duration={500}
+          >
+            <ArrowUpwardIcon className="arrowUpwardIcon" fontSize="30px" />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/Experience.js
+++ src/Experience.js
@@ -13,95 +13,97 @@ import "./Experience.css";
 
 function Experience() {
   return (
-    <div className="experience" id="experience">
-      <div className="experience__header">
-        <span style={{ color: "royalblue", fontSize: "25px" }}>02.</span>
-        <h1 style={{ paddingLeft: "15px", paddingRight: "15px" }}>
-          Experience
-        </h1>
-        <span className="titleLine"></span>
+    <div className="main__experience">
+      <div className="experience" id="experience">
+        <div className="experience__header">
+          <span style={{ color: "royalblue", fontSize: "25px" }}>02.</span>
+          <h1 style={{ paddingLeft: "15px", paddingRight: "15px" }}>
+            Experience
+          </h1>
+          <span className="titleLine"></span>
+        </div>
+        <Accordion className="accordion">
+          {/* <div className="work"> */}
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton className="title">
+                Freelance Developer
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="panel">
+              <h3 className="company">Murad Webdev Inc</h3>
+              <p className="date">Jan 2018 - April 2020</p>
+              <ul className="experience__item">
+                <li>
+                  Worked with a team of three designers to build a marketing
+                  website and e-commerce platform for blistabloc, an ambitious
+                  startup originating from Northeastern
+                </li>
+                <li>
+                  Helped solidify a brand direction for blistabloc that spans
+                  both packaging and web
+                </li>
+                <li>
+                  Interfaced with clients on a weekly basis, providing
+                  technological expertise
+                </li>
+              </ul>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton className="title">
+                Project Engineer
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="panel">
+              <h3 className="company">W.E. Lyons Construction</h3>
+              <p className="date">Jan 2018 - April 2020</p>
+              <ul className="experience__item">
+                <li>
+                  Worked with a team of three designers to build a marketing
+                  website and e-commerce platform for blistabloc, an ambitious
+                  startup originating from Northeastern
+                </li>
+                <li>
+                  Helped solidify a brand direction for blistabloc that spans
+                  both packaging and web
+                </li>
+                <li>
+                  Interfaced with clients on a weekly basis, providing
+                  technological expertise
+                </li>
+              </ul>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton className="title">
+                MWD Engineer
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="panel">
+              <h3 className="company">Halliburton</h3>
+              <p className="date">Jan 2018 - April 2020</p>
+              <ul className="experience__item">
+                <li>
+                  Worked with a team of three designers to build a marketing
+                  website and e-commerce platform for blistabloc, an ambitious
+                  startup originating from Northeastern
+                </li>
+                <li>
+                  Helped solidify a brand direction for blistabloc that spans
+                  both packaging and web
+                </li>
+                <li>
+                  Interfaced with clients on a weekly basis, providing
+                  technological expertise
+                </li>
+              </ul>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
       </div>
-      <Accordion className="accordion">
-        {/* <div className="work"> */}
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton className="title">
-              Freelance Developer
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel className="panel">
-            <h3 className="company">Murad Webdev Inc</h3>
-            <p className="date">Jan 2018 - April 2020</p>
-            <ul className="experience__item">
-              <li>
-                Worked with a team of three designers to build a marketing
-                website and e-commerce platform for blistabloc, an ambitious
-                startup originating from Northeastern
-              </li>
-              <li>
-                Helped solidify a brand direction for blistabloc that spans both
-                packaging and web
-              </li>
-              <li>
-                Interfaced with clients on a weekly basis, providing
-                technological expertise
-              </li>
-            </ul>
-          </AccordionItemPanel>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton className="title">
-              Project Engineer
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel className="panel">
-            <h3 className="company">W.E. Lyons Construction</h3>
-            <p className="date">Jan 2018 - April 2020</p>
-            <ul className="experience__item">
-              <li>
-                Worked with a team of three designers to build a marketing
-                website and e-commerce platform for blistabloc, an ambitious
-                startup originating from Northeastern
-              </li>
-              <li>
-                Helped solidify a brand direction for blistabloc that spans both
-                packaging and web
-              </li>
-              <li>
-                Interfaced with clients on a weekly basis, providing
-                technological expertise
-              </li>
-            </ul>
-          </AccordionItemPanel>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton className="title">
-              MWD Engineer
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel className="panel">
-            <h3 className="company">Halliburton</h3>
-            <p className="date">Jan 2018 - April 2020</p>
-            <ul className="experience__item">
-              <li>
-                Worked with a team of three designers to build a marketing
-                website and e-commerce platform for blistabloc, an ambitious
-                startup originating from Northeastern
-              </li>
-              <li>
-                Helped solidify a brand direction for blistabloc that spans both
-                packaging and web
-              </li>
-              <li>
-                Interfaced with clients on a weekly basis, providing
-                technological expertise
-              </li>
-            </ul>
-          </AccordionItemPanel>
-        </AccordionItem>
-      </Accordion>
     </div>
   );
 }
